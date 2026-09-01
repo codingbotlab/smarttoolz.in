329fb20498e20308677180c5c30751cfdf09eaba
CodingBot <133736959+codingbotlab@users.noreply.github.com>
2026-09-01 18:24:55 +0530

    Integrate Learning Hub promotion into SmartToolz homepage

--- smart-toolz/index.php
+++ smart-toolz/index.php
@@ -1,3 +1,16 @@
 <?php
 declare(strict_types=1);
+
+ob_start();
 require_once __DIR__ . '/home.php';
+$html = ob_get_clean();
+
+$promoFile = __DIR__ . '/learning-hub-promo.php';
+if (is_file($promoFile)) {
+    ob_start();
+    require $promoFile;
+    $promo = ob_get_clean();
+    $html = str_ireplace('</body>', $promo . "\n</body>", $html);
+}
+
+echo $html;

7c87146ebac7280f5f78175ebb353fa56f809d8a
CodingBot <133736959+codingbotlab@users.noreply.github.com>
2026-09-02 03:11:11 +0530

    Add QR Remotion composition

---
+++ Reddott-films/knowledge-videos/qr-generator/remotion/src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Composition, Audio, staticFile } from 'remotion';
+import { Video } from './Video.jsx';
+
+export const RemotionRoot = () => (
+  <Composition
+    id="QRGenerator"
+    component={Video}
+    durationInFrames={300 * 30}
+    fps={30}
+    width={1280}
+    height={720}
+  />
+);

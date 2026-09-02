0b2618fef83b060bfb42cf3dae7ab220fb52d73c
CodingBot <133736959+codingbotlab@users.noreply.github.com>
2026-09-02 21:29:46 +0530

    Remove duplicate Learning Hub promo from homepage bottom

--- smart-toolz/index.php
+++ smart-toolz/index.php
@@ -4,14 +4,6 @@ ob_start();
 require_once __DIR__ . '/home.php';
 $html = ob_get_clean();
 
-$promoFile = __DIR__ . '/learning-hub-promo.php';
-if (is_file($promoFile)) {
-    ob_start();
-    require $promoFile;
-    $promo = ob_get_clean();
-    $html = str_ireplace('</body>', $promo . "\n</body>", $html);
-}
-
 $youtubeUrl = getenv('SMARTTOOLZ_YOUTUBE_URL') ?: 'https://www.youtube.com/';
 
 $sitePromo = '<div class="st-site-promo" role="region" aria-label="SmartToolz site promotion">'

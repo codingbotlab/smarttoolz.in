e79ded9e6c005da325648633e69c849a628c648d
CodingBot <133736959+codingbotlab@users.noreply.github.com>
2026-09-04 00:59:44 +0530

    Route Keddy OAuth through registered YouTube callback

--- keddy-bot/oauth.php
+++ keddy-bot/oauth.php
@@ -2,15 +2,12 @@
 declare(strict_types=1);
 session_start();
 require __DIR__.'/bot.php';
-$c=cfg();
-$c['google_client_id']='523738407628-o1c4t43j4vjriajnvojpt4cio4mktr01.apps.googleusercontent.com';
-$c['redirect_uri']='https://smarttoolz.in/keddy-bot/oauth-callback.php';
-if(empty($c['google_client_secret'])) die('Configure the Keddy Google Client Secret first.');
+$c=googleCredentials();
 $state=bin2hex(random_bytes(24));
 $_SESSION['keddy_oauth_state']=$state;
 $url='https://accounts.google.com/o/oauth2/v2/auth?'.http_build_query([
-    'client_id'=>$c['google_client_id'],
-    'redirect_uri'=>$c['redirect_uri'],
+    'client_id'=>$c['id'],
+    'redirect_uri'=>$c['redirect'],
     'response_type'=>'code',
     'scope'=>'https://www.googleapis.com/auth/youtube.force-ssl',
     'access_type'=>'offline',

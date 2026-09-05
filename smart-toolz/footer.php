b4b17fb1f468958f4dbcce68fa0aad2be9ca5cb9
CodingBot <133736959+codingbotlab@users.noreply.github.com>
2026-09-05 20:53:18 +0530

    Show generated tool tutorial at the bottom of tool pages

--- smart-toolz/footer.php
+++ smart-toolz/footer.php
@@ -1,5 +1,6 @@
 <?php
 // Shared SmartToolz footer dependencies.
+if (is_file(__DIR__ . '/tutorial-embed.php')) require __DIR__ . '/tutorial-embed.php';
 ?>
 <link rel="stylesheet" href="/core/ui/app.css">
 <script defer src="/core/ui/app.js"></script>

05ea13e6451d0bddf7e4e7e2473ddcdfef7c4520
CodingBot <133736959+codingbotlab@users.noreply.github.com>
2026-09-07 02:12:55 +0530

    Add shared SmartToolz document head

---
+++ head.php
@@ -0,0 +1,7 @@
+<?php
+declare(strict_types=1);
+require_once __DIR__ . '/bootstrap.php';
+?>
+<link rel="preconnect" href="https://cdn.jsdelivr.net">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+<link rel="stylesheet" href="/assets/css/smarttoolz.css">

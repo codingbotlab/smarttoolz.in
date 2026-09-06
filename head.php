7d5bb0571f799eafa957b4f4aeb29bc9672dc639
CodingBot <133736959+codingbotlab@users.noreply.github.com>
2026-09-07 02:17:56 +0530

    Load shared tool page styles

--- head.php
+++ head.php
@@ -5,3 +5,4 @@ require_once __DIR__ . '/bootstrap.php';
 <link rel="preconnect" href="https://cdn.jsdelivr.net">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 <link rel="stylesheet" href="/assets/css/smarttoolz.css">
+<link rel="stylesheet" href="/assets/css/tool-pages.css">

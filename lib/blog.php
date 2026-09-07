ae5909a279d3c1ac60be82d3808a9d81575bde51
CodingBot <133736959+codingbotlab@users.noreply.github.com>
2026-09-07 13:38:55 +0530

    Add automatic blog thumbnail metadata

--- lib/blog.php
+++ lib/blog.php
@@ -11,7 +11,12 @@ function smarttoolz_blogs(): array
         $slug = basename($file, '.php');
         if (!preg_match('/^[a-z0-9]+(?:-[a-z0-9]+)*$/', $slug)) continue;
         $title = ucwords(str_replace('-', ' ', $slug));
-        $posts[] = ['slug'=>$slug, 'title'=>$title, 'url'=>'/blog/'.$slug.'/'];
+        $posts[] = [
+            'slug' => $slug,
+            'title' => $title,
+            'url' => '/blog/'.$slug.'/',
+            'thumbnail' => '/blog/'.$slug.'.svg'
+        ];
     }
     usort($posts, static fn($a,$b) => strcmp($b['slug'], $a['slug']));
     return $posts;

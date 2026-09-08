5ae7997d635e69e4a7d291eda0d49b99bef95e5e
CodingBot <133736959+codingbotlab@users.noreply.github.com>
2026-09-08 23:51:35 +0530

    Remove old SmartToolz blog theme

--- wordpress/wp-content/themes/smarttoolz-blog/searchform.php
+++
@@ -1,5 +0,0 @@
-<form role="search" method="get" class="search-form" action="<?php echo esc_url(home_url('/')); ?>">
-<label class="screen-reader-text" for="smarttoolz-search"><?php esc_html_e('Search for:', 'smarttoolz-blog'); ?></label>
-<input type="search" id="smarttoolz-search" class="search-field" placeholder="<?php echo esc_attr_x('Search…', 'placeholder', 'smarttoolz-blog'); ?>" value="<?php echo get_search_query(); ?>" name="s" />
-<button type="submit" class="search-submit"><?php esc_html_e('Search', 'smarttoolz-blog'); ?></button>
-</form>

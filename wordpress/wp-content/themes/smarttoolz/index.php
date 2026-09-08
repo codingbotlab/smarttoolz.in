79e29ab9b01f9ad5c232ecdebb719a42733b6497
CodingBot <133736959+codingbotlab@users.noreply.github.com>
2026-09-08 23:40:01 +0530

    Remove unused theme: smarttoolz

--- wordpress/wp-content/themes/smarttoolz/index.php
+++
@@ -1,1 +0,0 @@
-<?php get_header(); ?><section class="content-area"><?php if (have_posts()): while(have_posts()): the_post(); ?><article <?php post_class(); ?>><h1><?php the_title(); ?></h1><?php the_post_thumbnail('large'); ?><div><?php the_content(); ?></div></article><?php endwhile; else: ?><p>No content found.</p><?php endif; ?></section><?php get_footer(); ?>

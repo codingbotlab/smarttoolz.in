b4c64f151edddac38ab4beec8f6e0c92710278c9
CodingBot <133736959+codingbotlab@users.noreply.github.com>
2026-09-09 22:43:32 +0530

    Force SmartToolz theme update for custom auth header

--- wordpress/wp-content/plugins/smarttoolz-video/includes/theme-installer.php
+++ wordpress/wp-content/plugins/smarttoolz-video/includes/theme-installer.php
@@ -22,7 +22,7 @@ function smarttoolz_video_theme_target_dir() {
 }
 
 function smarttoolz_video_theme_source_version() {
-    return '1.2.1';
+    return '1.2.2';
 }
 
 function smarttoolz_video_theme_source_files() {

31d9722a7c90ea7d1d70b80eb193eafb1d9c1210
CodingBot <133736959+codingbotlab@users.noreply.github.com>
2026-09-09 22:58:27 +0530

    Bump SmartToolz theme version to force header and full-width fixes

--- wordpress/wp-content/plugins/smarttoolz-video/includes/theme-installer.php
+++ wordpress/wp-content/plugins/smarttoolz-video/includes/theme-installer.php
@@ -22,7 +22,7 @@ function smarttoolz_video_theme_target_dir() {
 }
 
 function smarttoolz_video_theme_source_version() {
-    return '1.2.3';
+    return '1.2.4';
 }
 
 function smarttoolz_video_theme_source_files() {

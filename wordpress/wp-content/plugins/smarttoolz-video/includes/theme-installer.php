33594b7fb6a26514bdfae278ad5c83055397d309
CodingBot <133736959+codingbotlab@users.noreply.github.com>
2026-09-09 21:02:10 +0530

    Bump bundled SmartToolz theme version

--- wordpress/wp-content/plugins/smarttoolz-video/includes/theme-installer.php
+++ wordpress/wp-content/plugins/smarttoolz-video/includes/theme-installer.php
@@ -22,7 +22,7 @@ function smarttoolz_video_theme_target_dir() {
 }
 
 function smarttoolz_video_theme_source_version() {
-    return '1.1.0';
+    return '1.2.0';
 }
 
 function smarttoolz_video_theme_source_files() {
